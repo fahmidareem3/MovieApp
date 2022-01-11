1b5921cc14b64686586a3b2c149e70f560c34919
fahmidareem3 <fahmidareem3@gmail.com>
2022-01-11 20:00:13 +0600

    search added

---
+++ script.js
@@ -0,0 +1,34 @@
+const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bd46198805a498241427c55f3eb0aea2&page1&page2';
+
+const IMG_PATH ='https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=bd46198805a498241427c55f3eb0aea2&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+//get initial movies
+getMovies(API_URL)
+
+
+async function  getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.results);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm != ''){
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''}
+
+    else{
+        window.location.reload()
+    }
+})
